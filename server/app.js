073f78dfd4478dd85825431d10e67ba2c1f8dc27
Abhishek Mogaveera <abmogaveer971999@gmail.com>
2020-06-07 01:46:56 +0530

    Hbs & Partials added :computer:

--- server/app.js
+++ server/app.js
@@ -1,7 +1,20 @@
+const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 const app = express();
+const publicPath = path.join(__dirname, '../public');
+const partialPath = path.join(__dirname, '../public/partials');
+app.set('view engine', 'hbs');
+app.set('views', publicPath);
+hbs.registerPartials(partialPath);
 app.get('', (req, res) => {
-  res.send('Hello Express');
+  res.render('index', { title: 'Weather App', author: 'Abhishek Mogaveera' });
+});
+app.get('/about', (req, res) => {
+  res.render('index', {
+    title: 'Weather App | About',
+    author: 'Abhishek Mogaveera',
+  });
 });
 app.listen(3000, () => {
   console.log('Server is listening on port 3000');
